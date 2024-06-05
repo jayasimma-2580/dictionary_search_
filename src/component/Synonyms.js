8674f74d27c820e58b6464e6098ba6f567728957
jayasimma-2580 <jayasimma2580@gmail.com>
2024-06-05 07:54:56 +0530

    Add files via upload

---
+++ src/component/Synonyms.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/Antonyms.css";
+export default function Synonyms(props) {
+  if (props.synonyms && props.synonyms.length > 0) {
+    const synonymsText = props.synonyms.join(", ");
+    return (
+      <div>
+        <span>Synonyms:</span> {synonymsText}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
